7ebc187b6a26713fc7c6ce735b3d85c16546cea8
Gautam Kumar Tiwary <tiwarygautam53@gmail.com>
2025-04-11 22:03:39 +0530

    a

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/studentDetails');
+    await mongoose.connect('mongodb+srv://tiwarygautam53:g83zB8e8j7ix7ZnQ@cluster0.zhfkjdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
