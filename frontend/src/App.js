1f5cc4ce9ea0928d3de7d00dd60e3590c580d992
Gautam Kumar Tiwary <tiwarygautam53@gmail.com>
2025-04-12 21:09:29 +0530

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function AppContent() {
       {isAuthenticated && <Navbar />}
       <div className="container">
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route
             path="/home"
