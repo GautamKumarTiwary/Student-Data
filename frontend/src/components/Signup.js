8b813392892a147dfc09bb855cea464a1b101850
Gautam Kumar Tiwary <tiwarygautam53@gmail.com>
2025-04-12 22:41:42 +0530

    register

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -29,7 +29,7 @@ const Signup = () => {
         }
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("https://student-data-2yz7.onrender.com/api/auth/createuser", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
